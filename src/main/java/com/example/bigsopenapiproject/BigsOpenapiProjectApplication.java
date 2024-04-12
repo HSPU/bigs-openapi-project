f713f3e486e586cadd39700b9c10bb4d8846247d
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-04-12 18:45:48 +0900

    first commit

---
+++ src/main/java/com/example/bigsopenapiproject/BigsOpenapiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigsopenapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigsOpenapiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigsOpenapiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bigsopenapiproject/BigsOpenapiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigsopenapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigsOpenapiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
