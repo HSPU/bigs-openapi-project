a8b9944d40d9bd8a62c3a8b59a6271038f3af7f9
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-04-14 20:55:03 +0900

    feat: ForecastApiController 내용 추가 - serviceKey 속성 값 생성 - apiUrl 속성 값 생성 - forecast 메소드 추가

--- src/main/java/com/example/bigsopenapiproject/domain/controller/ForecastApiController.java
+++ src/main/java/com/example/bigsopenapiproject/domain/controller/ForecastApiController.java
@@ -1,5 +1,11 @@
 package com.example.bigsopenapiproject.domain.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -7,17 +13,29 @@ import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ForecastApiController {
-    public static void main(String[] args) throws IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=서비스키"); /*Service Key*/
+
+    @Value("${serviceKey}")
+    private String serviceKey;
+
+    @Value("${apiUrl}")
+    private String apiUrl;
+
+    @GetMapping("/forecast")
+    public String forecast() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder(apiUrl); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + serviceKey); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("20", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode("20240413", "UTF-8")); /*‘21년 6월 28일발표*/
+        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode("20240413", "UTF-8")); /*‘24년 4월 13일발표*/
         urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0500", "UTF-8")); /*05시 발표*/
         urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("62", "UTF-8")); /*예보지점의 X 좌표값*/
         urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("130", "UTF-8")); /*예보지점의 Y 좌표값*/
+
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -36,6 +54,9 @@ public class ForecastApiController {
         }
         rd.close();
         conn.disconnect();
+        String result = sb.toString();
         System.out.println(sb.toString());
+
+        return result;
     }
 }
