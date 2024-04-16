fdb37f9b343314051ea064cdf6126d593eab3474
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-04-16 21:16:27 +0900

    feat: Weather 추가 - Weather entity 추가

---
+++ src/main/java/com/example/bigsopenapiproject/domain/entity/Weather.java
@@ -0,0 +1,24 @@
+package com.example.bigsopenapiproject.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Weather {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String baseDate;
+    private String baseTime;
+    private String category;
+    private String fcstDate;
+    private String fcstTime;
+    private String fcstValue;
+    private int nx;
+    private int ny;
+}
